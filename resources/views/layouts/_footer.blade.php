e96536907188436786a8745c8716090a7a5e9f60
zhoubohan <m18618474796@163.com>
2018-05-25 05:50:54 +0000

    finish basic page layout

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            众生皆苦，唯你独甜。
+        </p>
+        <p class="pull-right">
+            <a href="">联系我们</a>
+        </p>
+    </div>
+</footer>
